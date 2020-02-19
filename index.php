ae989487a0e4a5ccafec2c96040b499bb0f63ea6
Mathieu <mathieu.lehan@gmail.com>
2020-02-19 09:55:48 +0100

    insertion du hash et du fingerprint en base + canard.jpg

--- index.php
+++ index.php
@@ -2,69 +2,80 @@
 
 include("Browser.php");
 include("Language.php");
+include("db.php");
 
-echo('<b>fonction native get_browser :</b><br><br>');
-print_r(get_browser());
+$fingerprint_raw = "";
 
 if(strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE)
-    echo 'Internet explorer';
+    $fingerprint_raw .= 'Internet explorer';
 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== FALSE) //For Supporting IE 11
-    echo 'Internet explorer';
+    $fingerprint_raw .= 'Internet explorer';
 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== FALSE)
-    echo 'Mozilla Firefox';
+    $fingerprint_raw .= 'Mozilla Firefox';
 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE)
-    echo '<br><br>Google Chrome';
+    $fingerprint_raw .= 'Google Chrome';
 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== FALSE)
-    echo "Opera Mini";
+    $fingerprint_raw .= "Opera Mini";
 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== FALSE)
-    echo "Opera";
+    $fingerprint_raw .= "Opera";
 elseif(strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== FALSE)
-    echo "Safari";
+    $fingerprint_raw .= "Safari";
 else
-    echo 'Something else';
+    $fingerprint_raw .= 'Something else';
 
 /* Nom, version, OS et userAgent */
-echo '<br><br><b>Nom, version, OS et userAgent du navigateur</b><br><br>';
 $browserInfo = Browser::detect();
 foreach ($browserInfo as $key => $value) {
-    echo "{$key} => {$value}<br><br>";
+    $fingerprint_raw .= "{$key} => {$value}";
 }
 
 /* Langages acceptés */
-echo '<b>Langages acceptés avec versions</b><br><br>';
 $languages = Language::get_accepted_languages();
 foreach ($languages as $key => $value) {
-    echo "{$key} => {$value}<br><br>";
+    $fingerprint_raw .= "{$key} => {$value}";
 }
 
 /* Page précédemment visitée */
 if(isset($_SERVER['HTTP_REFERER'])) {
     $pagePrécédente = $_SERVER['HTTP_REFERER'];
     echo "<b>Page précédemment visitée : </b>$pagePrécédente";
+    $fingerprint_raw .= $pagePrécédente;
 }
 else
 {
-    echo 'HTTP_REFERER pas envoyé par le navigateur !';
+   // echo 'HTTP_REFERER pas envoyé par le navigateur !';
 }
 
-/* Résolution d'écran */
-echo '<br><br><b>Résolution de l\'écran : </b>';
-session_start();
-if (isset($_SESSION['screen_width']) AND isset($_SESSION['screen_height'])) {
-    echo $_SESSION['screen_width'] . 'x' . $_SESSION['screen_height'];
-} else if (isset($_REQUEST['width']) AND isset($_REQUEST['height'])) {
-    $_SESSION['screen_width'] = $_REQUEST['width'];
-    $_SESSION['screen_height'] = $_REQUEST['height'];
-    header('Location: ' . $_SERVER['PHP_SELF']);
+$fingerprint_hash = md5($fingerprint_raw, false);
+$sql = "SELECT * FROM fingerprint WHERE fingerprint_hash = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$fingerprint_hash]);
+$hash = $stmt->fetch();
+
+if($hash == null) {
+    $sql = "INSERT INTO fingerprint (fingerprint_raw, fingerprint_hash, first_visit) VALUES (:fingerprint_raw, :fingerprint_hash, CURRENT_TIMESTAMP)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':fingerprint_raw', $fingerprint_raw);
+    $stmt->bindParam(':fingerprint_hash', $fingerprint_hash);
+    $stmt->execute();
+}
+else {
+    $sql = "SELECT first_visit FROM fingerprint WHERE fingerprint_raw = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$fingerprint_raw]);
+    $raws = $stmt->fetch();
+
+    if ($raws == null) {
+        $sql = "INSERT INTO fingerprint (fingerprint_raw, fingerprint_hash, first_visit) VALUES (:fingerprint_raw, :fingerprint_hash, CURRENT_TIMESTAMP)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':fingerprint_raw', $fingerprint_raw);
+        $stmt->bindParam(':fingerprint_hash', $fingerprint_hash);
+        $stmt->execute();
+    }
+    else {
+        $derniereVisite = array_values($raws)[0];
+        echo("<br><br>Vous êtes déjà venu sur ce site le : $derniereVisite ");
+    }
 }
 
-/* Viewport (résolution affichée du site) */
-echo '<br><br><b>Hauteur et largeur affichables : </b><br>';
-$hautScrollbar = '<script type="text/javascript">document.write(window.innerHeight);</script>';
-$largScrollbar = '<script type="text/javascript">document.write(window.innerWidth);</script>';
-$hautNoScrollbar = '<script type="text/javascript">document.write(document.documentElement.clientHeight);</script>';
-$largNoScrollbar = '<script type="text/javascript">document.write(document.documentElement.clientWidth);</script>';
-echo "Hauteur disponible sans ascenseur : $hautScrollbar<br>";
-echo "Largeur disponible sans ascenseur : $largScrollbar<br>";
-echo "Hauteur disponible avec ascenseur : $hautNoScrollbar<br>";
-echo "Largeur disponible avec ascenseur : $largNoScrollbar<br>";
+echo '<img src="canard.jpg" alt="coin">';
